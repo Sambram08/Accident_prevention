73f9692701bc49c01a9a5ce5e794da7456a8ed7e
Sambram08 <162282454+Sambram08@users.noreply.github.com>
2025-03-30 18:04:06 +0530

    tah

--- build.sh
+++ build.sh
@@ -1,28 +1,21 @@
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
-
-# pip install -r requirements.txt
-
-# python manage.py collectstatic --no-input
-# python manage.py migrate
-# if [[ $CREATE_SUPERUSER ]];
-# then
-#   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-# fi
-
-
-
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -o errexit  
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
 
 # Install dependencies
-pip install --upgrade pip
 pip install -r requirements.txt
-gdown https://drive.google.com/uc?id=dyEekxHzR0Nyod9Qgn4DGA6tQXfKfGaks -O accidentprediction/model/xgboost_model.pkl
-# Apply Django migrations
+
+# Create directory for model if it doesn't exist
+mkdir -p accidentprediction/model/
+
+# Download the model file from Google Drive
+# Option 1: Using direct download link (if you have one)
+# curl -L "YOUR_DIRECT_DOWNLOAD_LINK" -o accidentprediction/model/xgboost_model.pkl
+
+# Option 2: Using gdown with correct file ID
+gdown https://drive.google.com/uc?id=dyEekxHzR0Nyod9Qgn4DGA6tQXfKfGaks -O accidentprediction/model/xgboost_model.pkl --no-cookies
+
+# Apply migrations
 python manage.py migrate
 
 # Collect static files
